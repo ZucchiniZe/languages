6d5f1a5a722eff9f181c6d645406004b11e87793
ZucchiniZe <me@alexb.ninja>
2015-01-05 17:06:49 -0800

    add links

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 $.getJSON('data.json', function(data) {
-  $('body').prepend('<div class="container"> <h1>GitHub Language Colors</h1> <p>This little experiment has taken the data from <code>languages.yml</code> and visualized it in a colorful way.</p> </div>')
+  $('body').prepend('<div class="container"> <h1>GitHub Language Colors</h1> <p>This little experiment has taken the data from <code>languages.yml</code> and visualized it in a colorful way.</p> <p>You can click on the language name to go to the trending for that language </p> </div>')
   for(var i=0; i < data.length; i++) {
     var c = data[i][1]
     c = c.substring(1);
@@ -10,9 +10,9 @@ $.getJSON('data.json', function(data) {
     var luminance = (r * 0.299 + g * 0.587 + b * 0.114) / 3;
 
     if (luminance < 40) {
-      $('#box').append('<div class="square" style="color:#EEE;background: ' + data[i][1] + '">' + data[i][0] + '</div>')
+      $('#box').append('<div class="square" style="color:#EEE;background:' + data[i][1] + '"><a class="light" href="https://github.com/trending?l=' + data[i][0] + '">' + data[i][0] + '</a></div>')
     } else {
-      $('#box').append('<div class="square" style="background: ' + data[i][1] + '">' + data[i][0] + '</div>')
+      $('#box').append('<div class="square" style="background:' + data[i][1] + '"><a class="dark" href="https://github.com/trending?l=' + data[i][0] + '">' + data[i][0] + '</a></div>')
     }
   }
 });
