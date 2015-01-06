fc79e0eb3c9d44d34a2675465f145570e7b28f48
ZucchiniZe <me@alexb.ninja>
2015-01-06 06:09:31 -0800

    fix it all

--- Rakefile
+++ Rakefile
@@ -1,4 +1,9 @@
-task :default => :generate
+require 'net/http'
+task :default => :download
+
+task :download do
+  File.write('languages.yml', Net::HTTP.get(URI.parse('https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml')))
+end
 
 task :generate do
   sh 'rm -rf data.json'
--- scraper.rb
+++ scraper.rb
@@ -1,14 +1,16 @@
 require 'yaml'
 
-file = YAML.load_file(Net::HTTP.get(URI.parse('https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml')))
+file = YAML.load_file('languages.yml')
 
 puts '['
 
-file.each do |lang|
-  if lang[1].has_key?('color') == true
-    puts "  [\"#{lang[0]}\", \"#{lang[1]['color']}\"],"
+file.each_with_index do |lang, i|
+  if i == file.size - 1
+    puts "  [\"#{lang[0]}\", \"#{lang[1]['color']}\"]"
   else
-    # p "#{lang[0]}: No color"
+    if lang[1].has_key?('color') == true
+      puts "  [\"#{lang[0]}\", \"#{lang[1]['color']}\"],"
+    end
   end
 end
 
