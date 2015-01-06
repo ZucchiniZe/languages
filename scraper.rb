02595a3c1648a6083b2e66ab63276edcc28e7240
ZucchiniZe <me@alexb.ninja>
2015-01-06 06:07:32 -0800

    Revert "add rakefile"
    This reverts commit 1461cf1af277eee5bfb5d0367f750fc64371cd96.
    
    Conflicts:
    	Rakefile

--- scraper.rb
+++ scraper.rb
@@ -4,13 +4,11 @@ file = YAML.load_file(Net::HTTP.get(URI.parse('https://raw.githubusercontent.com
 
 puts '['
 
-file.each_with_index do |lang, i|
-  if i == file.size - 1
-    puts "  [\"#{lang[0]}\", \"#{lang[1]['color']}\"]"
+file.each do |lang|
+  if lang[1].has_key?('color') == true
+    puts "  [\"#{lang[0]}\", \"#{lang[1]['color']}\"],"
   else
-    if lang[1].has_key?('color') == true
-      puts "  [\"#{lang[0]}\", \"#{lang[1]['color']}\"],"
-    end
+    # p "#{lang[0]}: No color"
   end
 end
 
